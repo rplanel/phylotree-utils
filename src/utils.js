d6005ca4c2cca2687c366adb152c52411137de5d
Remi Planel <rplanel@genoscope.cns.fr>
2017-08-09 17:05:44 +0200

    Add pre-order and reduce post-order functions

--- src/utils.js
+++ src/utils.js
@@ -10,26 +10,53 @@ export default function (children) {
 	return d.children;
     }
     
+    function loopChildren(node, callback) {
+	const children = getChildren(node);
+	if (children) {
+	    children.forEach(callback);
+	}
+    }
+    
     // PUBLIC FUNCTION
     /**
-     * 
+     * Invokes the specified function for node and each descendant in post-order traversal, 
+     * such that a given node is only visited after all of its descendants have already been visited.
      */
-    self.traverseTreePostOrder = function(root, callback) {
-	const children = getChildren(root);
-	if (children) {
-	    children.forEach(function(child){
-		self.traverseTreePostOrder(child, callback);
-	    });
-	}
+    self.eachAfter = function(root, callback) {
+	const cb = function(child){
+	    self.eachAfter(child, callback);
+	};
+	loopChildren(root, cb);
 	callback(root);
     };
+
+    self.reduceAfter = function(node, callback, acc) {
+	const cb = function(child){
+	    acc = self.reduceAfter(child, callback, acc);
+	};
+	loopChildren(node, cb);
+	return callback(acc, node);
+    };
+
+    
+
+    self.eachBefore = function(node, callback) {
+	const cb = function(child){
+	    self.eachBefore(child, callback);
+	};
+	callback(node);
+	loopChildren(node, cb);
+    };
+    
+    
+    
     
     /**
      *
      */
     self.addParent = function (root) {
 	root.parent = null;
-	var cb = function( node ){
+	const cb = function( node ){
 	    const children = getChildren(node);
 	    if (children) {
 		children.forEach(function(child){
@@ -37,7 +64,7 @@ export default function (children) {
 		});
 	    }
 	};
-	self.traverseTreePostOrder(root, cb);
+	self.eachAfter(root, cb);
     };
     
     return self;
--- test/utils.js
+++ test/utils.js
@@ -4,35 +4,28 @@ var phylotree_utils = require("../");
 
 
 
-tape("Tree post order traversal", function(test){
-    
-    var tree = {
-	id: 1,
-	children: [
-	    {
-		id: 2,
-		children: [
-		    {
-			id: 4
-		    },
-		    {
-			id: 5
-		    }
-		]
-	    },
-	    {
-		id: 3
-	    }
-	]
-    };
+tape("Tree post order traversal (eachAfter)", function(test){
+    var tree = {id: 1, children: [ {id: 2,children: [ {id: 4}, {id: 5} ]}, {id: 3}]};
     var node_ids_traversal = [];
     phylotree_utils
 	.utils()
-	.traverseTreePostOrder(tree,function(node){
+	.eachAfter(tree,function(node){
 	    node_ids_traversal.push(node.id);
 	});
     var traversal_signature = node_ids_traversal.join(";");
     test.equal(traversal_signature,"4;5;2;3;1");
+    test.end();
+});
+tape("Tree pre order traversal (eachBefore)", function(test){
+    var tree = {id: 1, children: [ {id: 2,children: [ {id: 4}, {id: 5} ]}, {id: 3}]};
+    var node_ids_traversal = [];
+    phylotree_utils
+	.utils()
+	.eachBefore(tree,function(node){
+	    node_ids_traversal.push(node.id);
+	});
+    var traversal_signature = node_ids_traversal.join(";");
+    test.equal(traversal_signature,"1;2;4;5;3");
     test.end();
 });
 tape("Chidlren accessor", function(test){
@@ -62,37 +55,19 @@ tape("Chidlren accessor", function(test){
     var node_ids_traversal = [];
     phylotree_utils
 	.utils(childAcc)
-	.traverseTreePostOrder(tree,function(node){
+	.eachAfter(tree,function(node){
 	    node_ids_traversal.push(node.id);
 	});
     var traversal_signature = node_ids_traversal.join(";");
     test.equal(traversal_signature,"4;5;2;3;1");
     test.end();
 });
-tape("Add parent function",function(test){
-    var tree = {
-	id: 1,
-	children: [
-	    {
-		id: 2,
-		children: [
-		    {
-			id: 4
-		    },
-		    {
-			id: 5
-		    }
-		]
-	    },
-	    {
-		id: 3
-	    }
-	]
-    };
+tape("Test addParent function",function(test){
+    var tree = {id:1,children:[{id: 2,children: [{id: 4},{id:5}]},{id:3}]};
     var node_ids_traversal = [];
     const TreeUtils = phylotree_utils.utils();
     TreeUtils.addParent(tree);
-    TreeUtils.traverseTreePostOrder(tree,function(node){
+    TreeUtils.eachAfter(tree,function(node){
 	if (node.parent) {
 	    node_ids_traversal.push(node.id+"_"+node.parent.id);
 	}
@@ -103,4 +78,31 @@ tape("Add parent function",function(test){
     var traversal_signature = node_ids_traversal.join(";");
     test.equal(traversal_signature,"4_2;5_2;2_1;3_1;1_null");
     test.end();
+});
+tape("Test reduceAfter", function(test){
+    var tree = {id: 1, children: [ {id: 2,children: [ {id: 4}, {id: 5} ]}, {id: 3}]};
+    const TreeUtils = phylotree_utils.utils();
+    
+    var sumAccCb = function(acc, curr) {
+	return acc + curr.id;
+    };
+    var sum = TreeUtils.reduceAfter(tree, sumAccCb, 0);
+    test.equal(sum,15);
+
+    var concat = TreeUtils.reduceAfter(tree, sumAccCb, "");
+    test.equal(concat,"45231");
+    
+    var concat_array_ids = function(acc, node) {
+	acc.push(node.id);
+	return acc;
+    };
+    var array_concat = [];
+    array_concat = TreeUtils.reduceAfter(tree, concat_array_ids, array_concat);
+    test.deepEqual(array_concat, [4, 5, 2, 3, 1], array_concat);
+
+
+    
+    test.end();
+    
+    
 });
